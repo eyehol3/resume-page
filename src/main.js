b2e25d9c406ff7500b4c83f5b8575c77cadaa97d
eyehol3 <eyeh0l3@gmail.com>
2021-02-22 16:07:27 +0200

    init

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import App from './App.svelte';
 
-import 'normalize.css';
-createApp(App).use(router).use(router).mount('#app')
+const app = new App({
+	target: document.body,
+	props: {
+		name: 'world'
+	}
+});
+
+export default app;
